06d5ae46954a16429534ce6aa34292f6daa5c943
magikarp01 <philliphguo@gmail.com>
2023-02-22 17:47:43 -0500

    made cerebras pruning method and iterative pruning

--- cerebras_pruning.py
+++ cerebras_pruning.py
@@ -1,4 +1,5 @@
-import torch.nn.utils.prune 
+import torch.nn.utils.prune as prune
+
 def get_module_name(param_name):
     if param_name[-5:] == ".bias":
         return param_name[:-5], "bias"
@@ -7,22 +8,40 @@ def get_module_name(param_name):
     else:
         return None, None
 
+default_opt_blacklist = ['model.decoder.embed_tokens', 'model.decoder.embed_positions']
+
 # mask the lowest magnitude weights of the whole model
-def mask_lowest(model, amount=.2):
+def mask_lowest(model, amount=.2, module_blacklist=default_opt_blacklist, prune_remove=True):
     module_dict = {}
     for n, m in model.named_modules():
         module_dict[n] = m
     # print(module_dict.keys())
     
     parameter_list = []
+    param_dict = {}
     for n, m in model.named_parameters():
         parameter_list.append(n)
+        param_dict[n] = m
     # print(parameter_list)
 
     for n in parameter_list:
         module_name, param_type = get_module_name(n)
-        if module_name is None or param_type is None or param_type=="bias":
+
+        # skip bias, embed, etc parameters
+        if module_name in default_opt_blacklist or module_name is None \
+            or param_type is None or param_type!="weight":
             continue
+
+        if len(param_dict[n]) < 2:
+            continue
+
         # perform the masking
         module = module_dict[module_name]
-        torch.nn.utils.prune.l1_unstructured(module=module, name=param_type, amount=.2)
+        # param_type should always be 'weight'
+        prune.l1_unstructured(module=module, name=param_type, amount=amount)
+
+        # remove mask, apply mask and make some weights 0
+        if prune_remove:
+            prune.remove(module=module, name=param_type)
+
+
