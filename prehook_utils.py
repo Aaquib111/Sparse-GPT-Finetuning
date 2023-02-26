6f86e10e4a4f6e89cd50f30ae9a944f2cf74d09a
Aaquib Syed <47124521+Aaquib111@users.noreply.github.com>
2023-02-26 12:26:33 -0500

    Add files via upload

---
+++ prehook_utils.py
@@ -0,0 +1,113 @@
+import torch
+from utils.hessian_utils import calc_hessian
+from collections import OrderedDict
+from typing import Dict, Callable
+
+# Generate forward pre hooks to record the input into features dict
+# Features is a dictionary for module inputs
+    # Key is module name
+    # Value is module input
+# Function does NOT actually modify features, only puts hooks that will eventually 
+# modify features when input is later fed into the model
+# specify device, have tensors on not cuda device so it doesn't hog vram
+# only store key, query, value proj inputs, out_proj inputs, 
+
+
+def get_feature_storage_name(param_name):
+    if 'k_proj' in param_name:
+        param_name = param_name.replace("k_proj", "in_proj")
+
+    elif 'v_proj' in param_name:
+        param_name = param_name.replace("v_proj", "in_proj")
+
+    elif 'q_proj' in param_name:
+        param_name = param_name.replace("q_proj", "in_proj")
+    return param_name
+
+
+# Function to check if param should be added to features dictionary
+def check_whitelist(param_name, whitelist):
+    for name_end in whitelist:
+        if name_end in param_name:
+            return True
+
+    return False
+
+
+opt_whitelist = ['self_attn.k_proj',
+'decoder.project_out',
+'decoder.project_in',
+# 'self_attn.v_proj',
+# 'self_attn.q_proj',
+'self_attn.out_proj',
+'fc1',
+'fc2']
+
+
+def put_input_hooks(model, features, feature_storage_device, verbose=False, whitelist=opt_whitelist):#, storage_dir, offload_freq=16
+
+    # Function to make a hook function that inserts input hessian into features dictionary
+    def get_features(name):
+
+        # pre_hook function that is input into nn.module.register_forward_pre_hook
+        def pre_hook(model, input):
+            if verbose:
+                try:
+                    print(f"for input {name}, shape is {input[0].shape}")
+                except:
+                    pass
+
+            # move tensors of input hessian to device, possibly store on different device with more memory
+            # check whitelist
+            if len(input) > 0 and check_whitelist(name, whitelist):
+                # get name as key to store in features (since k_proj, q_proj, v_proj have same input)
+                storage_name = get_feature_storage_name(name)
+                #print(storage_name)
+                # check if flattened
+                if len(input[0].shape) == 2:
+                    input_hessian = calc_hessian(torch.transpose(input[0], 0, 1), flattened=True).cpu()
+                # not flattened
+                else:
+                    input_hessian = calc_hessian(torch.transpose(input[0], 1, 2), flattened=False).cpu()
+                
+                if storage_name in features:
+                    features[storage_name] += input_hessian
+                # make new entry if not existing
+                else:
+                    features[storage_name] = input_hessian
+                del input_hessian
+                torch.cuda.empty_cache()
+                '''if features[storage_name].shape[0] % offload_freq == 0:
+                    if os.path.exists(f'{storage_dir}/{name}'):
+                        existing_tensor = torch.load(f'{storage_dir}/{name}')
+                        new_tensor = torch.cat((existing_tensor, features[name]), dim=0)
+                        torch.save(new_tensor,f'{storage_dir}/{name}')
+                    # make new entry if not existing
+                    else:
+                        torch.save(features[storage_name],f'{storage_dir}/{name}')
+                    del features[storage_name]'''
+
+        
+        # return the pre_hook function that will be fed into register_forward_pre_hook
+        return pre_hook
+    
+    all_hooks = []
+    # call get_features, put in hooks at every module
+    for n, m in model.named_modules():
+        hook_func = get_features(n)
+        # print(m)
+        new_hook = m.register_forward_pre_hook(hook_func)
+        all_hooks.append(new_hook)
+
+    return all_hooks
+
+def remove_all_hooks(model: torch.nn.Module) -> None:
+    for name, child in model._modules.items():
+        if child is not None:
+            if hasattr(child, "_forward_hooks"):
+                child._forward_hooks: Dict[int, Callable] = OrderedDict()
+            elif hasattr(child, "_forward_pre_hooks"):
+                child._forward_pre_hooks: Dict[int, Callable] = OrderedDict()
+            elif hasattr(child, "_backward_hooks"):
+                child._backward_hooks: Dict[int, Callable] = OrderedDict()
+            remove_all_hooks(child)
---
+++ save_utils.py
@@ -0,0 +1,116 @@
+# LOAD PRUNED MODEL
+
+import torch
+from torch.nn.utils import prune
+from tqdm import tqdm
+
+#device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+# function to get module name from parameter name
+def get_module_name(param_name):
+    if param_name[-5:] == ".bias":
+        return param_name[:-5], "bias"
+    elif param_name[-7:] == ".weight":
+        return param_name[:-7], "weight"
+    
+    elif param_name[-10:] == ".bias_orig":
+        return param_name[:-10], "bias"
+    elif param_name[-12:] == ".weight_orig":
+        return param_name[:-12], "weight"
+    else:
+        return None, None
+    
+# load model without masks
+def load_unmasked_model(existing_model, state_dict_path):
+    existing_model.load_state_dict(torch.load(state_dict_path))
+
+# prune 0s to a mask, to make training easier (ostensibly)
+class ZeroPruning(prune.BasePruningMethod):
+    PRUNING_TYPE = "unstructured"
+
+    # default threshold is 0, prunes weights that are already 0 (for training)
+    def __init__(self):
+        pass
+
+    def compute_mask(self, tensor, default_mask):
+        return torch.abs(tensor) != 0
+
+# apply pytorch mask in place of 0 weights to make backpropagation easier for training
+default_opt_blacklist = ['module.model.decoder.embed_tokens', 'module.model.decoder.embed_positions']
+def mask_from_pruned(model, module_blacklist=default_opt_blacklist):
+    module_dict = {}
+    for n, m in model.named_modules():
+        module_dict[n] = m
+    # print(module_dict.keys())
+    
+    parameter_list = []
+    param_dict = {}
+    for n, m in model.named_parameters():
+        parameter_list.append(n)
+        param_dict[n] = m
+    # print(parameter_list)
+
+    for n in parameter_list:
+        module_name, param_type = get_module_name(n)
+
+        # skip bias, embed, etc parameters
+        if module_name in module_blacklist or module_name is None \
+            or param_type is None or param_type!="weight":
+            continue
+
+        if len(param_dict[n].shape) < 2:
+            continue
+
+        ZeroPruning.apply(module=module_dict[module_name], name=param_type)
+        
+# load model with masks
+def load_masked_model(existing_model, state_dict_path):
+
+    # first load like normal
+    load_unmasked_model(existing_model, state_dict_path)
+    
+    # then reapply the (previously removed) masks
+    mask_from_pruned(model=existing_model)
+    
+def load_masked_model_single(existing_model, state_dict_path):
+    #print('in')
+    #print(torch.cuda.is_initialized())
+    state_dict = torch.load(state_dict_path, map_location=torch.device('cpu'))
+    #print(torch.cuda.is_initialized())
+    if "module" in list(state_dict.keys())[0]:
+        state_dict = {k.replace('module.',''): v for k, v in state_dict.items()}
+    #print(torch.cuda.is_initialized())
+    existing_model.load_state_dict(state_dict)
+    #print(torch.cuda.is_initialized())
+    # then reapply the (previously removed) masks
+    mask_from_pruned(model=existing_model)
+    #print('out')
+    
+# unmask model with 0s in place
+def unmask_model(model, module_blacklist=default_opt_blacklist):
+    module_dict = {}
+    for n, m in model.named_modules():
+        module_dict[n] = m
+    # print(module_dict.keys())
+    
+    parameter_list = []
+    param_dict = {}
+    for n, m in model.named_parameters():
+        parameter_list.append(n)
+        param_dict[n] = m
+    # print(parameter_list)
+
+    for n in parameter_list:
+        module_name, param_type = get_module_name(n)
+
+        # skip bias, embed, etc parameters
+        if module_name in module_blacklist or module_name is None \
+            or param_type is None or param_type!="weight":
+            continue
+
+        if len(param_dict[n].shape) < 2:
+            continue
+        
+        prune.remove(module=module_dict[module_name], name=param_type)
+        torch.cuda.clear_cache()
