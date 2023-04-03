e4cb02002fa05b4fa464be54e8ef715a74481b4f
Aaquib <aaquibahm@gmail.com>
2023-04-03 16:11:25 -0400

    Clean Up

--- sbatch_params.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --mem=256G
-#SBATCH --time=0-24:00:00
-#SBATCH --partition=gpu
-#SBATCH --nodelist=gpu-102
-
-jupyter execute Testing.ipynb
--- utils/test_utils.py
+++ utils/test_utils.py
@@ -5,7 +5,7 @@ from datasets import load_dataset
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from utils.save_utils import load_unmasked_model, load_masked_model, unmask_model, mask_from_pruned
+from save_pruned_model import load_unmasked_model, load_masked_model, unmask_model, mask_from_pruned
 import gc
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu' 
@@ -51,3 +51,48 @@ def test_model(model_name, encodings, token_length, seq_len, stride, wandb, SPAR
     del loaded_model
     gc.collect()
     torch.cuda.empty_cache()
+
+
+def finetune_model(model_name, tokenizer, SPARSITY, device=device, EPOCH_COUNT=10):
+    #encode tokens
+    def encode_tok(examples):
+        return tokenizer(examples['text'], truncation=True, padding='max_length')
+
+    #stream c4, training split
+    training_data = load_dataset('wikitext', 'wikitext-2-raw-v1', split='train', streaming=True)
+    #IMPORTANT: process data while streaming -> remove unnecessary columns in batches
+    training_data = training_data.map(encode_tok, batched=True, remove_columns=["text"])
+
+    #set data to tensor mode
+    training_data = training_data.with_format("torch")
+
+    #dataloader from dataloader (mlm=False when training without mask)
+    reformatted_data = DataLoader(training_data, collate_fn=DataCollatorForLanguageModeling(tokenizer, mlm=False))
+    
+    loaded_model = OPTForCausalLM.from_pretrained(f'facebook/{model_name}',
+                                                  output_attentions=True,
+                                                  output_hidden_states=True).to(device=device) # type: ignore
+    loaded_model = torch.nn.DataParallel(loaded_model, device_ids=[0,1,2,3])# activate masks
+    
+    if SPARSITY != 1:
+        load_masked_model(loaded_model, f'pruned_models/{model_name}-{SPARSITY}.pt')
+    loaded_model.eval()
+    _ = loaded_model(torch.randint(high=20, size=(1,10)))
+    mask_from_pruned(model=loaded_model)
+    #training loop
+    loaded_model.train().to(device)
+    t_optim = torch.optim.AdamW(params=loaded_model.parameters(), lr=1e-5)
+    for epoch in tqdm(range(EPOCH_COUNT)):
+        training_data.set_epoch(epoch)
+        for i, batch in enumerate(reformatted_data):
+            if i == 5:
+                break
+            batch = {k: v.to(device) for k, v in batch.items()}
+            outputs = loaded_model(**batch)
+            loss = outputs[0]
+            loss.backward()
+            t_optim.step()
+            t_optim.zero_grad()
+    unmask_model(loaded_model)
+    torch.save(loaded_model.state_dict(), f'pruned_models/{model_name}-{SPARSITY}-finetuned.pt')
+
